c593fdeb46cbb14ae1ce11c48949526496913549
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2025-11-24 15:56:14 -0500

    modified main.py to adhere to new system Merged jacob-mainwrapup into code-cleanup

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
 import json
+import llm_pipeline.benchmark as bm
 
 def setup():
     #check if database exists
     if not os.path.exists('./data/input/dataset'):
-        import data.input.parquet_creation as pc
-        pc.create_dataset()
-        pc.split_parquet("./data/input/dataset.parquet", "./data/input/dataset")
-        #remove big dataset
-        if os.path.exists("./data/input/dataset.parquet"):
-            os.remove("./data/input/dataset.parquet")
+        import dataset_pipeline.dataset_creation as dc
+        dc.pipeline()
+    
+'''    config = {}
+    with open('./config.json','r')as fp:
+        config = json.load(fp)
+    if os.path.exists('./notice.bak'):
+        with open('./notice.bak','r') as fp:
+            config["start"] = int(fp.readline())
+    return config'''
 
 if __name__ == '__main__':
-    import data.output.response_handler as rh
-    config = setup()
-    rh.output_pipeline(config)
+    setup()
+    bm.pipeline()
     
     

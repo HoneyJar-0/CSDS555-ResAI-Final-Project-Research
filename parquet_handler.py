b421600d7fd3d83d088e05464c826712e2a12b66
ShafathZ <shafath.zaman@gmail.com>
2025-11-24 19:54:44 -0500

    Completely fixed and refactored to new config system except for start and end index. Also added working tensorboard code

--- parquet_handler.py
+++ parquet_handler.py
@@ -3,15 +3,15 @@ import os
 import time
 import pyarrow.dataset as ds
 
+from configs import experiment_config
+
 
 class BatchWriter:
-    def __init__(self, model_name, worker_name, output_dir="./data/output", buffer_size=100):
-        self.output_dir = os.path.join(output_dir, f"model_{model_name.strip()}", f"user_{worker_name.strip()}")
+    def __init__(self):
+        self.output_dir = os.path.join(experiment_config.output_dir, f"model_{experiment_config.model_id.strip()}", f"user_{experiment_config.worker_name.strip()}")
 
         self.buffer = []
-        self.buffer_size = buffer_size
         self.file_counter = 0
-        self.model_name = model_name
 
         os.makedirs(self.output_dir, exist_ok=True)
 
@@ -19,15 +19,15 @@ class BatchWriter:
         self.buffer.append({
             "UUID": uuid,
             "response": response,
-            "model": self.model_name
+            "model": experiment_config.model_id
         })
 
-        if len(self.buffer) % self.buffer_size == 0:
+        if len(self.buffer) % experiment_config.buffer_check_count == 0:
             self._check_size()
 
     def _check_size(self):
         df_temp = pd.DataFrame(self.buffer)
-        if df_temp.memory_usage(deep=True).sum() >= 90 * 1024 * 1024:
+        if df_temp.memory_usage(deep=True).sum() >= experiment_config.max_parquet_size_mib * 1024 * 1024:
             print("Memory Full, Flushing Data")
             self.flush()
 

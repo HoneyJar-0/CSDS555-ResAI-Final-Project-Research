6ef2674dd1f22ebe8c272bbd3c2645aabf11231d
REZ3LIET <rz.samar.kale@gmail.com>
2026-03-02 16:25:42 -0500

    implementation of vLLM in pipeline

---
+++ llm_pipeline/benchmark.bk.py
@@ -0,0 +1,146 @@
+import os
+import re
+import gc
+from datetime import datetime
+
+import torch
+import psutil
+import pandas as pd
+import port_forwarding
+
+from tqdm import tqdm
+from unsloth import FastLanguageModel
+from torch.utils.tensorboard import SummaryWriter
+
+from parquet_handler import BatchWriter
+from llm_pipeline.data_loader import PromptDataLoader
+
+from configs import experiment_config
+
+process = psutil.Process(os.getpid())
+
+class Benchmark:
+    def __init__(self):
+        self.model_name = self.get_model_name(experiment_config.model_id)
+        self.model, self.tokenizer = self.load_model()
+
+        data = PromptDataLoader()
+        self.loader = data.load_parquet_to_df(batch_size=experiment_config.batch_size)
+        self.writer = BatchWriter()
+
+        if experiment_config.tensorboard_active:
+            self.tensorboard = SummaryWriter(log_dir=os.path.join(experiment_config.log_dir, datetime.now().strftime(experiment_config.log_format)))
+            port_forwarding.launch_tensorboard()
+
+
+    def get_model_name(self, model_id: str) -> str:
+        model_dict = {
+            "llama": "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit",
+            "gemma": "unsloth/gemma-3-12b-it-unsloth-bnb-4bit",
+            "mistral": "unsloth/mistral-7b-instruct-v0.3-bnb-4bit"
+        }
+        if model_id.strip().lower() not in model_dict.keys():
+            raise ValueError(f"Invalid Model Name, expected {list(model_dict.keys())} got {model_id}")
+
+        return model_dict[model_id]
+
+    def load_model(self):
+        """Loads model_id"""
+        gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
+
+        model, tokenizer = FastLanguageModel.from_pretrained(
+            model_name = self.model_name,
+            max_seq_length = experiment_config.max_seq_length,
+            dtype = experiment_config.model_dtype,
+            load_in_4bit = experiment_config.load_in_4bit,
+        )
+
+        tokenizer.padding_side = "left"
+        tokenizer.truncation_side = "left"
+        #tokenizer.pad_token = tokenizer.eos_token
+
+        if "gemma" in self.model_name:
+            from unsloth.chat_templates import get_chat_template
+            tokenizer = get_chat_template(
+                tokenizer,
+                chat_template = "gemma-3",
+            )
+
+        return model, tokenizer
+
+    def run(self) -> str:
+        """
+        Runs the benchmark
+        """
+        # Load the model for inferencing
+        FastLanguageModel.for_inference(self.model)  # Enable native 2x faster inference
+
+        for i,batch in tqdm(enumerate(self.loader), total=len(self.loader), desc="Processing Batches"):
+            chat_prompts = [
+                self.tokenizer.apply_chat_template(
+                    conv,
+                    tokenize=False,
+                    add_generation_prompt=True
+                )
+                for conv in batch[1]
+            ]
+
+            # Create prompts for inputs for batch
+            inputs = self.tokenizer(
+                chat_prompts,
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+            ).to("cuda")
+
+            # Inference on batch
+            outputs = self.model.generate(
+                **inputs,
+                max_new_tokens=experiment_config.max_new_tokens,
+                do_sample=False,
+                temperature=experiment_config.temperature,
+                use_cache=True
+            )
+
+            decoded = self.tokenizer.batch_decode(outputs)
+
+            # Post process batch
+            cleaned = []
+            if "mistral" in self.model_name.lower():
+                cleaned = [r.replace("[INST]", "").replace("<s>", "").replace("</s>", "").strip().split("[/INST]")[-1] for r in decoded]
+            elif "llama" in self.model_name.lower():
+                for idx, r in enumerate(decoded):
+                    cleaned_r = r.split("<|start_header_id|>assistant<|end_header_id|>")[-1].strip().split('\n', 1)[-1].replace("<|eot_id|>","")
+                    if not cleaned_r:
+                        raise ValueError(f"Over pruned string found in Decoded for llama model. UUID: {batch[0][idx]} Original String:\n{r}")
+
+                    cleaned.append(cleaned_r)
+
+            if not cleaned:
+                raise ValueError(f"Empty output. Check Decoded:\n{decoded}")
+
+            # Store aligned with original IDs
+            notice_id = 0
+            for id, out in zip(batch[0], cleaned):
+                self.writer.add(id, out.strip())
+                notice_id = id
+
+            if experiment_config.tensorboard_active and i%experiment_config.log_interval == 0:
+                self.tensorboard.add_scalar("Progress/Percentage", i/len(self.loader)*100, i)
+                self.tensorboard.add_scalar("Current UUID", batch[0][0], i)
+                self.tensorboard.add_scalar("GPU VRAM Allocated (GB)", torch.cuda.memory_allocated(0) / 1e9, i)
+                self.tensorboard.add_scalar("CPU RAM Allocated (GB)", process.memory_info().rss / 1e9, i)
+            print(f"Flushed batch {i} to {self.writer.flush()}")
+            with open('./notice.txt', 'w')as fp:
+                fp.write(str(notice_id))
+        # Final flush to save remaining data
+        out_path = self.writer.flush()
+        return out_path
+
+def pipeline():
+    benchmark = Benchmark().run()
+
+if __name__ == "__main__":
+    pipeline()
--- llm_pipeline/test.py
+++ llm_pipeline/test.py
@@ -1,45 +1,59 @@
 import time
+import gc
+import torch
 from vllm import LLM, SamplingParams
 
 def main():
     prompts = [
         # "Hello, my name is",
-        "The president of the United States is",
         # "The capital of France is",
-        # "The future of AI is",
+        "The future of AI is",
     ]
-    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+    sampling_params = SamplingParams(temperature=0.0, max_tokens=256)
     print(f"Sampling Params: {sampling_params}")
 
-    model = "mistralai/Mixtral-8x7B-Instruct-v0.1"  # google/gemma-3-12b-it, meta-llama/Llama-3.1-8B-Instruct, mistralai/Mixtral-8x7B-Instruct-v0.1, Qwen/Qwen3-8B
-    quantization = None  # None, bitsandbytes
+    model_dict = {
+        "llama": "meta-llama/Llama-3.1-8B-Instruct",
+        "gemma": "google/gemma-3-12b-it",
+        "mixtral": "mistralai/Mixtral-8x7B-Instruct-v0.1",
+        "qwen": "Qwen/Qwen3-8B"
+    }
+    quantization = "bitsandbytes"  # None, bitsandbytes
+    for model in model_dict.values():
+        gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
 
-    m_time = time.time()
-    llm = LLM(
-        model=model,                            # model taken from HuggingFace
-        # tokenizer=model,                        # what tokenizer to use
-        # dtype="int4",
-        # tokenizer_mode=tokenizer,               # default is auto; slow is here only for comparison
-        # tensor_parallel_size=1,                 # Splits the model over n GPUs to fit larger models. WARNING!!! Greatly increases latency in communication
-        quantization=quantization,              # Specify type of quantization method wanted. See vLLM docs for specifics
-        # gpu_memory_utilization=0.9,             # Default value is 90%; mentioned here in case of OOM errors. Specifies how much VRAM to reserve for the model
-        # max_num_seqs=batch_size                 # Effectively, the batch size. 256 is the default. Decrease to avoid OOM errors if necessary
-    )
-    load_time = time.time() - m_time
+        print("\n\n", "*"*30, f" {model} ", "*"*30)
+        m_time = time.time()
+        llm = LLM(
+            model=model,                                # model taken from HuggingFace
+            enforce_eager=True,                         # Disables CUDA graphs which records history of sorts
+            max_model_len=256,
+            # tokenizer=model,                          # what tokenizer to use
+            # dtype="int4",
+            # tokenizer_mode=tokenizer,                 # default is auto; slow is here only for comparison
+            # tensor_parallel_size=1,                   # Splits the model over n GPUs to fit larger models. WARNING!!! Greatly increases latency in communication
+            quantization=quantization,                  # Specify type of quantization method wanted. See vLLM docs for specifics
+            # gpu_memory_utilization=0.9,               # Default value is 90%; mentioned here in case of OOM errors. Specifies how much VRAM to reserve for the model
+            max_num_seqs=5                   # Effectively, the batch size. 256 is the default. Decrease to avoid OOM errors if necessary
+        )
+        load_time = time.time() - m_time
 
-    gen_time = []
-    for _ in range(3):
-        s_time = time.time()
-        outputs = llm.generate(prompts, sampling_params)
-        gen_time.append(time.time() - s_time)
+        gen_time = []
+        print("\n\n", "="*30, f" {model} ", "="*30)
+        for _ in range(3):
+            s_time = time.time()
+            outputs = llm.generate(prompts, sampling_params)
+            gen_time.append(time.time() - s_time)
+            
+            for output in outputs:
+                prompt = output.prompt
+                generated_text = output.outputs[0].text
+                print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
 
-        for output in outputs:
-            prompt = output.prompt
-            generated_text = output.outputs[0].text
-            print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
-
-    print(f"{model} load time: {load_time}")
-    print(f"Average Inference time for quantization: {quantization} over 3 runs: {sum(gen_time)/len(gen_time)}")
+        print(f"{model} load time: {load_time}")
+        print(f"Average Inference time for quantization: {quantization} over 3 runs: {sum(gen_time)/len(gen_time)}")
     print("over")
 
 if __name__ == "__main__":
